19c73343a905b2ec6ba7126ba6e6b7ac710feda2
Aditya Mehta <adumehta03@gmail.com>
2021-06-11 11:33:16 +0530

    Add files via upload

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -5,42 +5,42 @@ function getTimeRemaining(endtime) {
     var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
     var days = Math.floor(t / (1000 * 60 * 60 * 24));
     return {
-      'total': t,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
     };
-  }
-  
-  function initializeClock(id, endtime) {
+}
+
+function initializeClock(id, endtime) {
     var clock = document.getElementById(id);
     var daysSpan = clock.querySelector('.days');
     var hoursSpan = clock.querySelector('.hours');
     var minutesSpan = clock.querySelector('.minutes');
     var secondsSpan = clock.querySelector('.seconds');
-  
+
     function updateClock() {
-      var t = getTimeRemaining(endtime);
-  
-      daysSpan.innerHTML = t.days;
-      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  
-      if (t.total <= 0) {
-        clearInterval(timeinterval);
-      }
+        var t = getTimeRemaining(endtime);
+
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
+        }
     }
-  
+
     updateClock();
     var timeinterval = setInterval(updateClock, 1000);
-  }
-  
-  // var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-  var eventTime = new Date('June 30, 2021 4:15:00');
+}
 
-  if(new Date() > eventTime){
+// var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+var eventTime = new Date('June 13, 2021 17:00:00');
+
+if (new Date() > eventTime) {
     var clock = document.getElementById('clockdiv');
     var daysSpan = clock.querySelector('.days');
     var hoursSpan = clock.querySelector('.hours');
@@ -51,11 +51,10 @@ function getTimeRemaining(endtime) {
     hoursSpan.innerHTML = '0';
     minutesSpan.innerHTML = '0';
     secondsSpan.innerHTML = '0';
-  
-  }
-  else{
 
-  var deadline = new Date(Date.parse( eventTime) );
-  initializeClock('clockdiv', deadline);
+} else {
+
+    var deadline = new Date(Date.parse(eventTime));
+    initializeClock('clockdiv', deadline);
 
 }
